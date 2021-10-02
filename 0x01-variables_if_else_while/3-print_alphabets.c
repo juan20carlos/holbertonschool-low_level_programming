69f57cc03971fc7c22073814c3133d9207737c08
juan20carlos <juancarlos14089@gmail.com>
2021-10-02 17:29:42 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C = C + 1;
+	}
+	putchar('\n');
+	return (0);
+}
