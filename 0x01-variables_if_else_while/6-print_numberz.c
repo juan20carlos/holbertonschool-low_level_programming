cc538c12372a734ab1f627fae3b7d5ef87e12b38
juan20carlos <juancarlos14089@gmail.com>
2021-10-03 00:55:20 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c  <= 57)
+	{
+	putchar(c);
+	c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
