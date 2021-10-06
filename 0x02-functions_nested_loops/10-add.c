a03631e67aa00b965fabcf3e91d02db2fce51f7c
juan20carlos <juancarlos14089@gmail.com>
2021-10-06 16:21:35 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - the adds two number
+ *
+ * @a: input
+ * @b: input
+ * Return: Always s success
+ */
+int add(int a, int b)
+{
+
+	int s;
+
+	s = a + b;
+	return (s);
+}
