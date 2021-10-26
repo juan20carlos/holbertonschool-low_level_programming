3ceb80f10f7310df5732fd281a8a88e3a7bb5c2a
juan20carlos <juancarlos14089@gmail.com>
2021-10-26 22:04:16 +0000

    Prototype

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *srt;
 
-	srt = malloc(sizeof(char) * size);
-
 	if (srt  == 0)
 	{
 		return (NULL);
 	}
+	srt = malloc(sizeof(char) * size);
 	if (srt == NULL)
 	{
 		return (NULL);
