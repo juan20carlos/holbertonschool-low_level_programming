30255ea40f96eeace4ee4cd94b355cd67006b905
juan20carlos <juancarlos14089@gmail.com>
2021-11-02 17:13:19 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+# ifndef _SAMPLE
+# define _SAMPLE
+/**
+ * struct dog - information about dogs
+ * @age: age of dog
+ * @owner: owner of dog
+ * @name: name dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+# endif
