cdc8a353982aa8e50e5fb6e6feb4bd0d5bfdc61f
juan20carlos <juancarlos14089@gmail.com>
2021-11-15 18:28:44 +0000

    Prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,12 @@
+#ifndef _JUAN_CARLOS_
+#define _JUAN_CARLOS_
 #include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * 
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -14,4 +16,4 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
-
+#endif
