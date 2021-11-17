447c233747f69c771c81a2ae278d7925f8f2a37c
juan20carlos <juancarlos14089@gmail.com>
2021-11-17 23:11:16 +0000

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - free all list
+ *
+ * @head: pointer to struct
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next_free;
+
+	while (current != NULL)
+	{
+		next_free = current->next;
+		free(current->str);
+		free(current);
+		current = next_free;
+	}
+}
