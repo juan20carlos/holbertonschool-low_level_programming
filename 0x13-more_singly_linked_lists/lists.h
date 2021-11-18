de1ecc46085c007fd172f4ffded70c9671955c49
juan20carlos <juancarlos14089@gmail.com>
2021-11-18 02:39:27 +0000

    Prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define _JUAN_CARLOS_
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,5 +19,5 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
