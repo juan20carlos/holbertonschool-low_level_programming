c4a19df98d910e83bee085a88726a9cb6bb09bff
juan20carlos <juancarlos14089@gmail.com>
2021-11-20 02:01:31 +0000

    Prototype

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - cambia el bit
+ *
+ * @n: puntero apunta a numero
+ * @index: indice de bit
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int numero;
+
+	if (index > sizeof(unsigned long int))
+	{
+		return (-1);
+	}
+	numero = 1 << index;
+	*n = *n | numero;
+	return (1);
+}
