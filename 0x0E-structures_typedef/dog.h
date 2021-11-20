0b157ad5172043bd237e7c33c8ca9c3cc761a111
juan20carlos <juancarlos14089@gmail.com>
2021-11-20 16:32:17 +0000

    Prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,12 @@
  * @owner: owner of dog
  * @name: name dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
