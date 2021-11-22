4a612f2749c6d9e063980f1547f31b8b82b69840
juan20carlos <juancarlos14089@gmail.com>
2021-11-22 20:00:26 +0000

    Prototype

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef _JUAN_CARLOS_
+#define _JUAN_CARLOS_
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
