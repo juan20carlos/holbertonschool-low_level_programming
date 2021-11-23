7a881f720e87f0c6e996cc6a2488b0a22e2e8b8c
juan20carlos <juancarlos14089@gmail.com>
2021-11-23 00:28:41 +0000

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,11 +34,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	escritura = write(fd, text_content, _strlen(text_content));
+	if (text_content != NULL)
+	{
+		escritura = write(fd, text_content, _strlen(text_content));
+	}
+	close(fd);
 	if (escritura == -1)
 	{
 		return (-1);
 	}
-	close(fd);
 	return (1);
 }
